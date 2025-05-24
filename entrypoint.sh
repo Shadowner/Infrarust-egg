b40231888aae0dfcdb35eebe9266a8afca35f4f8
Shadowner <12647984+Shadowner@users.noreply.github.com>
2025-05-24 10:05:38 +0200

    Feat : Working Egg !

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,60 @@ function handle_signal {
   kill -$1 "$child_pid" 2>/dev/null
 }
 
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+
+# Prepare the proxies
+
+# check if the folder proxies doesn't exists
+if [ ! -d "/home/container/proxies" ]; then
+    # create the folder
+    mkdir /home/container/proxies
+fi
+
+function createProxyYml {
+
+    local proxyNumber=$1
+    local domains="DOMAINS_${proxyNumber}"
+    local addresses="ADDRESSES_${proxyNumber}"
+
+    # Check if the env variable is set
+    if [ -z "${!domains}" ]; then
+        echo "The domains variable is not set. Please set it to the $1 domain of the proxy"
+        exit 1
+    fi
+
+    # Check if the env variable is set
+    if [ -z "${!addresses}" ]; then
+        echo "The addresses env variable is not set. Please set it to the $1 domain of the proxy"
+        exit 1
+    fi
+
+    # If file exists, delete it
+    if [ -f "/home/container/proxies/$1.yml" ]; then
+        rm /home/container/proxies/$1.yml
+    fi
+    echo "addresses:" >>"/home/container/proxies/$1.yml"
+    echo "${!addresses}" | while IFS=',' read -r address; do
+        echo "  - $address" >>"/home/container/proxies/$1.yml"
+    done
+}
+
+
+# Check if PROXY_COUNT is set if not assume the User configured everything manually
+if [ -n "$PROXY_COUNT" ]; then
+    # Check if it's a number
+    if ! [[ $PROXY_COUNT =~ ^[0-9]+$ ]]; then
+        echo "The PROXY_COUNT env variable is not a number"
+        exit 1
+    fi
+
+        # Loop through the number of proxies and create the yml files
+    for i in $(seq 1 $PROXY_COUNT); do
+        createProxyYml $i
+    done
+fi
+
+
 # Set up signal traps for Unix environment (Docker)
 trap 'handle_signal TERM' TERM
 trap 'handle_signal INT' INT
@@ -13,7 +67,7 @@ trap 'handle_signal USR1' USR1
 trap 'handle_signal USR2' USR2
 
 # Echo and start the command
-${STARTUP} &
+${MODIFIED_STARTUP} &
 
 # Store child PID
 child_pid=$!
